ebc5ccd3d85325d5be1f33d9e8d111fa28b0a8c2
Viktoria-eir <139718564+Viktoria-eir@users.noreply.github.com>
2025-08-03 19:41:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const button = document.querySelector('#btn');
+button.addEventListener('click', calculateAmount);
+
+const buttonTip = document.querySelector('#addTip');
+buttonTip.addEventListener('click', showTip);
+
+function showTip(e) {
+    e.preventDefault();
+    tip.style.display = 'block';
+}
+
+function calculateAmount(e) {
+    e.preventDefault();
+
+    const bill = document.querySelector('#bill').value;
+    const people = document.querySelector('#people').value;
+    const tip = document.querySelector('#tip').value;
+
+    if (bill === '' || people === '' || people < 1) {
+        Swal.fire({
+            icon: "error",
+            title: "Error!",
+            text: "Please enter your information!"
+        });
+        return;
+    }
+
+    const amontPerPerson = bill / people;
+    const tipPerPerson = (bill * tip) / people;
+    const totalSum = amontPerPerson + tipPerPerson;
+
+    document.querySelector('#dividedBill').textContent = amontPerPerson.toFixed(2);
+    document.querySelector('#dividedTip').textContent = tipPerPerson.toFixed(2);
+    document.querySelector('#billAndTip').textContent = totalSum.toFixed(2);
+}
+
+
+
+document.getElementById("orderOptionsway").addEventListener("click", () => {
+    Swal.fire("✅ Takeaway selected!", "We'll prepare your order for pickup!", "success");
+})
